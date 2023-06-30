6675798d65e239f6c5a4642172a587830c8d05d6
kevin <kevindiegodasilvasousa@gmail.com>
2023-06-30 00:40:27 -0300

    fix: Updated favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,21 +6,22 @@ class MyDocument extends Document {
     return (
       <Html lang={siteMetadata.language} className="scroll-smooth">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
-          <link
+          {/* <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" /> */}
+          {/* <link
             rel="icon"
             type="image/png"
             sizes="32x32"
             href="/static/favicons/favicon-32x32.png"
-          />
-          <link
+          /> */}
+          {/* <link
             rel="icon"
             type="image/png"
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          /> */}
+          {/* <link rel="manifest" href="/static/favicons/site.webmanifest" /> */}
+          {/* <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" /> */}
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico" type="image/x-icon" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
